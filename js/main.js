7e9bc437078c366340795e9b2046aef9ace25483
Mason <chtuff@gmail.com>
2024-03-11 19:11:31 -0400

    got the war function to kind of work

--- js/main.js
+++ js/main.js
@@ -49,11 +49,10 @@ function drawTwo(){
      
     }else{
       document.querySelector('#results').innerText = 'Time for War'
-      // document.querySelector('button').addEventListener('click', timeForWar)
-
+       // $('h4:contains('Time for War')').add('button[id="warButton"]');
     }
-  
-
+    
+    
   })
   .catch(err => {
     console.log(`error ${err}`)
@@ -75,34 +74,43 @@ function convertFaceCardsToNum(val){
   }
 }
 
-// // function timeForWar(){
-// //   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`
-// //   fetch(url)
-// //   .then(res => res.json())
-// //   .then(data => {
-// //     console.log(data)
+document.getElementById('warButton').addEventListener('click', timeForWar)
+
+ function timeForWar(){
+   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`
+   fetch(url)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data)
         
-// //     if(player1Val > player2Val){
-// //       document.querySelector('#results').innerText = 'Player 1 Wins'
-
-// //       player1CardCount += 4
-// //       document.querySelector('#cardCountPlayer1').innerText = player1CardCount
-// //       player2CardCount -= 4
-       
-// //       localStorage.setItem('Player1', player1CardCount)
-// //       }else if(player1Val < player2Val){
-// //       document.querySelector('#results').innerText = 'Player 2 Wins'
+     document.querySelector('#player1').src = data.cards[0].image
+     document.querySelector('#player2').src = data.cards[1].image
+     let player1Val = convertFaceCardsToNum(data.cards[0].value)
+     let player2Val = convertFaceCardsToNum(data.cards[1].value)
+      //  player1CardCount += 4
+      //  player2CardCount += 4
       
-// //       player2CardCount += 1
-// //       document.querySelector('#cardCountPlayer2').innerText = player2CardCount
-// //       player1CardCount -= 1
-
-// //       localStorage.setItem('Player2', player2CardCount)
-// //     }
+      if(player1Val < player2Val){
+        document.querySelector('#results').innerText = 'Player 2 Wins War'
+        player2CardCount += 4
+        player1CardCount -= 4
+    
+      }else{
+        document.querySelector('#results').innerText = 'Player 1 Wins War'
+        player1CardCount += 4
+        player2CardCount -= 4
+    
+      }
+      
+      
+      //  document.querySelector('#cardCountPlayer1').innerText = player1CardCount
+      //  document.querySelector('#cardCountPlayer2').innerText = player2CardCount
+     
   
-// //   })
-// //   .catch(err => {
-// //     console.log(`error ${err}`)
-// //   });
-
-// }
+    })
+      .catch(err => {
+          console.log(`error ${err}`)
+     });
+  }
+  
+  
